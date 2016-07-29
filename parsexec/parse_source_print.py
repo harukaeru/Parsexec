2fc8048d554e0cf49163fc3d6494d4b395f92112
Nakada Takumi <Twitter@KaeruVim>
2016-07-29 11:40:27 +0900

    Beautiful

--- parsexec/parse_source_print.py
+++ parsexec/parse_source_print.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python
 import sys
 import re
+try:
+    from colorama import Fore, Style
+    GREEN = Fore.GREEN
+    RED = Fore.RED
+    RESET = Style.RESET_ALL
+except ImportError:
+    GREEN = RED = RESET = ''
 
 
 IMPORT_CLASS_FILE_NAME = __file__.replace(
     'parse_source_print', 'import_classes'
 )
+print_list = []
+
+
+def remove_indent(s):
+    lines = s.split('\n')
+    firstline = lines[0]
+    matched = re.match(r'^( +).*', firstline)
+    if matched:
+        space = matched.groups()[0]
+        new_lines = [re.sub('^' + space, '', line) for line in lines]
+        return '\n'.join(new_lines)
+    else:
+        return s
+
+
+def colorprint(*args, **kwargs):
+    if len(args) > 1:
+        def p():
+            print(*args, **kwargs)
+        print_list.append(p)
+    else:
+        data = args[0]
+
+        def q():
+            print(GREEN + "data:" + RESET, data)
+            print(GREEN + "class:" + RESET, type(data))
+        print_list.append(q)
 
 
 def main():
-    try:
-        from colorama import Fore, Style
-        GREEN = Fore.GREEN
-        RED = Fore.RED
-        RESET = Style.RESET_ALL
-    except ImportError:
-        GREEN = RED = RESET = ''
-
     with open(IMPORT_CLASS_FILE_NAME) as f:
         lines = [l.replace('\n', '') for l in f.readlines()]
         s, e = 0, 2
@@ -45,33 +71,8 @@ def main():
             first_indent = groups[0]
             source = first_indent + source
 
-    def remove_indent(s):
-        lines = s.split('\n')
-        firstline = lines[0]
-        matched = re.match(r'^( +).*', firstline)
-        if matched:
-            space = matched.groups()[0]
-            new_lines = [re.sub('^' + space, '', line) for line in lines]
-            return '\n'.join(new_lines)
-        else:
-            return s
-
     source = remove_indent(source)
-    _print = print
-    print_list = []
 
-    def print(*args, **kwargs):
-        if len(args) > 1:
-            def p():
-                _print(*args, **kwargs)
-            print_list.append(p)
-        else:
-            data = args[0]
-
-            def q():
-                _print(GREEN + "data:" + RESET, data)
-                _print(GREEN + "class:" + RESET, type(data))
-            print_list.append(q)
     source_for_test = '\n{}'.format(source)
     error = True
     import_statements = ''
@@ -80,11 +81,11 @@ def main():
             print_list = []
             exec(import_statements + source_for_test)
 
-            _print(GREEN + "----- Out -----" + RESET)
+            print(GREEN + "----- Out -----" + RESET)
             for p in print_list:
                 p()
-            _print(RED + '----- source -----' + RESET)
-            _print(import_statements + source)
+            print(RED + '----- source -----' + RESET)
+            print(import_statements + source)
             error = False
         except NameError as e:
             # TODO: printed(twice or more)
